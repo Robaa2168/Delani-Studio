190394ca9e94931ffecc3d937b65fbeb9e7c5fef
Robaa2168 <71884172+Robaa2168@users.noreply.github.com>
2020-10-13 14:45:48 +0300

    Update script.js
    updated javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-$("p").hover(function(){
-  $(this).css("background-color", "yellow");
-  }, function(){
-  $(this).css("background-color", "pink");
-});
+
+  $(document).ready(function() {
+    $(".load").hide();
+    $(".contain-f").hover(function() {
+      $(this).find(".load").toggle(600);
+    });
+
+  });
+  $(".design").click(function() {
+    $(".image3").slideToggle();
+    $(".de").slideToggle();
+  });
+
+  $(".develop").click(function() {
+    $(".image1").slideToggle(1100);
+    $(".dev").slideToggle(1200);
+  });
+
+  $(".product").click(function() {
+    $(".image2").slideToggle(1100);
+    $(".pro").fadeToggle(1200);
+  });
