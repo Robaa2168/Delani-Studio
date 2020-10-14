84b40835de258e1ae0f56a0368d17b18a5f353f7
Robaa2168 <71884172+Robaa2168@users.noreply.github.com>
2020-10-14 07:52:28 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
 
-  $(document).ready(function() {
-    $(".load").hide();
-    $(".contain-f").hover(function() {
-      $(this).find(".load").toggle(600);
-    });
+
+$(document).ready(function(){
+  $(".col-md-3").hover(function(){
+    $("#ontario").toggleClass("hovereffect");
 
   });
+});
+  $(".col-md-3").hover(function(){
+    $("#studio").toggleClass("hovereffect");
+
+  });
+    $(".col-md-3").hover(function(){
+      $("#jim").toggleClass("hovereffect");
+
+    });
+      $(".col-md-3").hover(function(){
+        $("#day").toggleClass("hovereffect");
+  });
+    $(".col-md-3").hover(function(){
+    $("#calculator").toggleClass("hovereffect");
+
+    });
+    $(".col-md-3").hover(function(){
+    $("#burned").toggleClass("hovereffect");
+
+    });
+  $(document).ready(function() {
   $(".design").click(function() {
     $(".image3").slideToggle();
     $(".de").slideToggle();
   });
-
+});
   $(".develop").click(function() {
-    $(".image1").slideToggle(1100);
-    $(".dev").slideToggle(1200);
+    $(".image1").slideToggle();
+    $(".dev").slideToggle();
   });
 
   $(".product").click(function() {
-    $(".image2").slideToggle(1100);
-    $(".pro").fadeToggle(1200);
+    $(".image2").slideToggle();
+    $(".pro").fadeToggle();
   });
